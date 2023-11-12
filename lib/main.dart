780afd5358c0be06444c0d2d1755f4cf2cbc474d
Navid Rahman <navid@hellokompass.com>
2023-11-12 15:57:24 +0600

    UI layout's updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_task/presentation/home_page.dart';
-import 'package:quiz_task/routes.dart';
+import 'package:quiz_task/router/routes.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/presentation/quiz_page.dart
+++ lib/presentation/quiz_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_task/constants/constants.dart';
 
 class QuizPage extends StatefulWidget {
   const QuizPage({super.key});
@@ -10,8 +11,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int selectedOption = -1; // Default value indicating no option selected
+
   @override
   Widget build(BuildContext context) {
+    // Get.put(QuizController());
     return Scaffold(
       appBar: AppBar(
         title: const Text('Quiz Page'),
@@ -20,9 +24,138 @@ class _QuizPageState extends State<QuizPage> {
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.blue, Colors.green], // Set your gradient colors
+            colors: [Colors.blue, Colors.green],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              /// Top Header
+              const Align(
+                alignment: Alignment.topRight,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Question 1 of 10',
+                      style: kSmallTextStyle,
+                    ),
+                    Text(
+                      'Score: 20',
+                      style: kSmallTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 30,
+              ),
+
+              /// Question and Answer Segment
+              Expanded(
+                child: Column(
+                  children: [
+                    const Column(
+                      children: [
+                        // Add Question here
+                        Text(
+                          'What is the capital of India? What is the capital of India? What is the capital of India?',
+                          style: kMediumTextStyle,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        // Add question marks here
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            '10 points',
+                            style: kSmallTextStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    // Add an image containing the full width of the screen, but not the full height
+                    // SizedBox(
+                    //   width: double.infinity,
+                    //   height: 200,
+                    //   child: Image.asset(
+                    //     'assets/your_image.jpg',
+                    //     fit: BoxFit.cover,
+                    //   ),
+                    // ),
+
+                    Column(
+                      children: [
+                        RadioListTile(
+                          title: const Text('Option A', style: kSmallTextStyle),
+                          value: 0,
+                          fillColor: MaterialStateProperty.all(Colors.white),
+                          groupValue: selectedOption,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedOption = value as int;
+                            });
+                          },
+                        ),
+                        RadioListTile(
+                          title: const Text(
+                            'Option B',
+                            style: kSmallTextStyle,
+                          ),
+                          value: 1,
+                          fillColor: MaterialStateProperty.all(Colors.white),
+                          groupValue: selectedOption,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedOption = value as int;
+                            });
+                          },
+                        ),
+                        RadioListTile(
+                          title: const Text(
+                            'Option C',
+                            style: kSmallTextStyle,
+                          ),
+                          value: 2,
+                          fillColor: MaterialStateProperty.all(Colors.white),
+                          groupValue: selectedOption,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedOption = value as int;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              /// Footer
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Next',
+                  style: kMediumTextStyle,
+                ),
+              ),
+            ],
           ),
         ),
       ),
